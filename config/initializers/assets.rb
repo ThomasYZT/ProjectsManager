3cc1407b8915294291aafdf81ce1b7dfa46cb63e
ThomasYZT <624524240@qq.com>
2016-08-26 20:38:30 +0800

    home_page

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,4 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( dashboard.js project_application.js project_apply.js project_search.js dynamic_management.js home_page.js)
+Rails.application.config.assets.precompile += %w( dashboard.js project_application.js project_apply.js project_search.js dynamic_management.js)
