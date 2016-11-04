2a4aaaa472cb46f6ddc863723230e67f2393c5a3
ThomasYZT <624524240@qq.com>
2016-11-04 10:39:39 +0800

    choosepoint

--- app/controllers/choose_point_controller.rb
+++ app/controllers/choose_point_controller.rb
@@ -4,7 +4,7 @@ class ChoosePointController < ApplicationController
 		url = "#{Rails.root}/lib/ftl"
 		name = ["county","projectname","numberone","numbertwo","numberthree","numberfour","nameone","nametwo","namethree","namefour","countryside","county","village","town","year","month","day"]
 		value = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17"]
-		Word.create_word(url,"e:\\",name,value)
+		Word.create_word(url,"e:\\\\",name,value)
 
 		#Test.test("1","2","3")
 	end
