76972fcc53a0fda9098c35eb5d03cff47fddd537
ThomasYZT <624524240@qq.com>
2016-11-04 10:44:49 +0800

    choosepoint

--- app/controllers/choose_point_controller.rb
+++ app/controllers/choose_point_controller.rb
@@ -5,7 +5,7 @@ class ChoosePointController < ApplicationController
 		name = ["county","projectname","numberone","numbertwo","numberthree","numberfour","nameone","nametwo","namethree","namefour","countryside","county","village","town","year","month","day"]
 		value = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17"]
 		Word.create_word(url,"e:\\\\",name,value)
-
+		puts "#{Rails.root}"
 		#Test.test("1","2","3")
 	end
 end
